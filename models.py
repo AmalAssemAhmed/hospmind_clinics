882b94672f368a69cddd1e2f958fbea2d8e6d8bf
AmalAssemAhmed <amalassemahmed@gmail.com>
2025-04-14 14:45:32 +0200

    Update models.py

--- models.py
+++ models.py
@@ -3,50 +3,72 @@ import tensorflow as tf
 import streamlit as st
 import os
 import requests
-#load ECG model
+
+# load ECG model
 @st.cache_resource
 def load_ecg_model():
     model_path = "ecg_model.h5"
     if not os.path.exists(model_path):
         url = "https://huggingface.co/AmalAssem/hospiminds_models/resolve/main/my_ecg_model.h5"
-        response = requests.get(url)
-        with open(model_path, "wb") as f:
-            f.write(response.content)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            with open(model_path, "wb") as f:
+                f.write(response.content)
+        except Exception as e:
+            st.error(f"error loading ECG model: {e}")
+            return None
     model = tf.keras.models.load_model(model_path, compile=False)
     return model
 
-#load chest X ray model
+# load chest X ray model
 @st.cache_resource
 def load_chest_model():
-    model_path = "chest_mdel.h5"
+    model_path = "chest_model.h5"
     if not os.path.exists(model_path):
         url = "https://huggingface.co/AmalAssem/hospiminds_models/resolve/main/chest_efficientb0_h5.h5"
-        response = requests.get(url)
-        with open(model_path, "wb") as f:
-            f.write(response.content)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            with open(model_path, "wb") as f:
+                f.write(response.content)
+        except Exception as e:
+            st.error(f"error loadind Chest X-Ray model: {e}")
+            return None
     model = tf.keras.models.load_model(model_path, compile=False)
     return model
 
-#load MRI Brain tumor ray model
+# load MRI Brain tumor model
 @st.cache_resource
 def load_MRI_model():
-    model_path = "MRI_mdel.h5"
+    model_path = "MRI_model.h5"
     if not os.path.exists(model_path):
         url = "https://huggingface.co/AmalAssem/hospiminds_models/resolve/main/brain_tumor.h5"
-        response = requests.get(url)
-        with open(model_path, "wb") as f:
-            f.write(response.content)
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            with open(model_path, "wb") as f:
+                f.write(response.content)
+        except Exception as e:
+            st.error(f"error loading brain tumor model: {e}")
+            return None
     model = tf.keras.models.load_model(model_path, compile=False)
     return model
-
-    
 
 @st.cache_resource
 def load_heartattack_model():
-    model = joblib.load("heartattack_xgb_best_model.pkl")
+    try:
+        model = joblib.load("heartattack_xgb_best_model.pkl")
+    except Exception as e:
+        st.error(f"error loading Heart Attack model: {e}")
+        return None
     return model
 
 @st.cache_resource
 def load_heartattack_scaler():
-    scaler = joblib.load("HeartAttack_scaler.pkl")
-    return scaler
+    try:
+        scaler = joblib.load("HeartAttack_scaler.pkl")
+    except Exception as e:
+        st.error(f"error loadig Heart Attack Scaler: {e}")
+        return None
+    return scale
