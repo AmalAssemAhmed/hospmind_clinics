ad9c51b8294cac02f9fea61c73ca1be388af321f
AmalAssemAhmed <amalassemahmed@gmail.com>
2025-04-15 15:37:53 +0200

    Update neurology.py

--- neurology.py
+++ neurology.py
@@ -206,14 +206,7 @@ def neurology_content(tumor_model):
         background-color: #1d2b1f;
         color: #a3f7bf;
     }
-     img {
-        border-radius: 30% !important;
-        width: 120px !important;
-        height: 120px !important;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-    }
+    
       
         </style>
         """, unsafe_allow_html=True)
