deb58428f54c0fac1079a10c60729d40286696bd
AmalAssemAhmed <amalassemahmed@gmail.com>
2025-04-22 22:06:47 +0200

    Update pulmonology.py

--- pulmonology.py
+++ pulmonology.py
@@ -327,10 +327,10 @@ def pulmonology_content(chest_model):
     
      cursor.execute(f"SELECT report_pdf FROM {table} WHERE national_id=?", (search_id,))
      result = cursor.fetchone()
-    # st.write( "path :",os.path.exists(result[0]))
+     st.write( "path :",os.path.exists(result[0]))
      st.write("Debug: result =", result)
     
-     if result is not None and os.path.exists(result[0]) is True and result[0]: :
+     if result is not None and os.path.exists(result[0]) is True and result[0]: 
             with open(result[0], "rb") as file:
                 st.download_button(label="📄 Download Report", data=file,file_name=f"chest_xray_report_{search_id}.pdf", mime="application/pdf")
        
